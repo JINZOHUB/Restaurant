670cdb087c1821bb4b583d542aba9b7e4a38ee3d
송재호 <David@songjaehos-MacBook-Pro.local>
2020-12-19 12:08:14 +0900

    rerenew

---
+++ shareRes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here,
+def index(request):
+    return HttpResponse("index")
+def restaurantDetail(request):
+    return HttpResponse("restaurantDetail")
+def restaurantCreate(request):
+    return HttpResponse("「estaurantC「eate")
+def categoryCreate(request):
+    return HttpResponse("categoryCreate")
